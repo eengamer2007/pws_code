52d402337143bd22f408248ea82b2515d956f0f8
eengamer2007 <eengamer2007@gmail.com>
2023-01-10 21:15:09 +0100

    did things

--- src/fft.rs
+++ src/fft.rs
@@ -1,4 +1,4 @@
-use rustfft::num_complex::Complex;
+use num::complex::Complex;
 use core::f64::consts::PI;
 
 use async_recursion::async_recursion    ;
@@ -9,7 +9,8 @@ fn sync_fft() {
 
 pub async fn fft(input: &[f64]) -> Vec<Complex<f64>> {
     fft_complex(
-        &(input.iter().enumerate().map(|x| Complex::new(x.0 as f64, *x.1)).collect::<Vec<Complex<f64>>>())
+        //&(input.iter().enumerate().map(|x| Complex::new(x.0 as f64, *x.1)).collect::<Vec<Complex<f64>>>())
+        &(input.iter().map(|x| Complex::new(*x, 0.)).collect::<Vec<Complex<f64>>>())
     ).await.to_vec()
 }
 
@@ -19,9 +20,12 @@ async fn fft_complex(x: &[Complex<f64>]) -> Vec<Complex<f64>> {
     if n <= 1 {
         return x.to_owned();
     }
-    let even = fft_complex(&x.iter().step_by(2).cloned().collect::<Vec<_>>()).await;
-    let odd  = fft_complex(&x.iter().skip(1).step_by(2).cloned().collect::<Vec<_>>()).await;
-
+    let iter1 = x.iter().step_by(2).cloned().collect::<Vec<_>>();
+    let iter2 = x.iter().skip(1).step_by(2).cloned().collect::<Vec<_>>();
+    let even_f = fft_complex(&iter1);
+    let odd_f  = fft_complex(&iter2);
+    let even = even_f.await;
+    let odd = odd_f.await;
     let mut temp = vec![Complex::new(0.0_f64, 0.0_f64); n];
     for k in 0..(n / 2) {
         temp[k] = even[k]
--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,136 @@
-use std::fs::File;
 use flac::StreamReader;
+use num::complex::ComplexFloat;
 use plotters::prelude::*;
+use std::fs::File;
+
+use std::io::Write;
+use std::time::Instant;
 
 mod fft;
 
-const DIV: u32 = 2;
+const DIV: u32 = 1;
 
-#[tokio::main(flavor = "multi_thread", worker_threads = 4)]
+fn flush() {
+    std::io::stdout().flush().unwrap();
+}
+
+fn save_graph(plot1: &[f64], plot2: &[f64], fname: &str, title: &str) -> Result<(), Box<dyn std::error::Error>> {
+    // make the drawing area
+    let root = SVGBackend::new(fname, ((plot1.len() + 40) as u32, 500)).into_drawing_area();
+    let (upper, lower) = root.split_vertically(200);
+    // fill drawing area with white
+    root.fill(&WHITE)?;
+
+    upper.titled(title, ("sans-serif", 20, &BLACK).into_text_style(&root))?;
+
+    // get max and min value
+    let mut max: f64 = 0.;
+    let mut min: f64 = 0.;
+    for i in plot1.iter() {
+        if i > &max {
+            max = *i
+        } else if i < &min {
+            min = *i
+        };
+    }
+
+    println!("min: {min}, max: {max}");
+
+    // set the margin, label size and make the grid and obtain grid
+    let mut chart1 = ChartBuilder::on(&upper)
+        .margin(5)
+        .x_label_area_size(30)
+        .y_label_area_size(55)
+        .build_cartesian_2d(0.0f64..(plot1.len() as f64), min..max)?;
+
+    let mut max: f64 = 0.;
+    let mut min: f64 = 0.;
+    for i in plot2.iter() {
+        if i > &max {
+            max = *i
+        } else if i < &min {
+            min = *i
+        };
+    }
+
+    let mut chart2 = ChartBuilder::on(&lower)
+        .margin(5)
+        .x_label_area_size(30)
+        .y_label_area_size(55)
+        .build_cartesian_2d(0.0f64..(plot2.len() as f64), min..max)?;
+
+    // configure the mesg
+    chart1.configure_mesh().draw()?;
+    chart2.configure_mesh().draw()?;
+
+    // draw the graph
+    chart1
+        .draw_series(LineSeries::new(
+            (0..(plot1.len())).map(|x| (x as f64, plot1[x as usize])),
+            &RED,
+        ))?
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 1, y)], &RED));
+
+    chart2
+        .draw_series(LineSeries::new(
+            (0..(plot2.len())).map(|x| (x as f64, plot2[x as usize])),
+            &RED,
+        ))?
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 1, y)], &RED));
+
+    // write the graph to file
+    root.present()?;
+    Ok(())
+}
+
+#[tokio::main(flavor = "multi_thread", worker_threads = 12)]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let start = Instant::now();
     
-    match StreamReader::<File>::from_file("./in/witch_doctor.flac") {
+    let r_min = 0;
+    let r_max = 2usize.pow(13);
+
+    let range = r_min..r_max;
+    match StreamReader::<File>::from_file("in/witch_doctor.flac") {
         Ok(mut stream) => {
             // get stream info
             let info = stream.info();
             println!("{info:?}");
-            // make plotters plot
-            println!("make file");
-            let name = format!("out/test{DIV}.png");
-            File::create(&name)?;
-            let root = BitMapBackend::new(&name, ((info.total_samples as u32 + 40) / 10_u32.pow(DIV), 140))
-                .into_drawing_area();
-            println!("fill");
-            root.fill(&WHITE)?;
-            println!("start chart");
-            let mut chart = ChartBuilder::on(&root)
-                .margin(5)
-                .x_label_area_size(30)
-                .y_label_area_size(30)
-                .build_cartesian_2d(0.0f64..(info.total_samples as f64), -1.0f64..1.0f64)?;
-                
-
-            println!("set mesh");
-            chart.configure_mesh().draw()?;
-
-
-            println!("start plot");
-            chart
-            .draw_series(LineSeries::new(
-                (-50..=50).map(|x| x as f64 / 50.0).map(|x| (x, x * x)),
-                &RED,
-            ))?
-            .label("y = x^2")
-            .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
-
-
-            println!("make vec");
-            let mut data: Vec<f64> = vec!();
+            print!("make vec");
+            flush();
             // iterate over all samples
-            for sample in stream.iter::<i16>() {
-                data.push(sample as f64 / std::i16::MAX as f64);
+            //for sample in stream.iter::<i16>() {
+            //    data.push(sample as f64 / std::i16::MAX as f64);
+            //}
+            /*
+            let data: Vec<f64> = stream
+                .iter::<i16>()
+                .map(|x| x as f64 / std::i16::MAX as f64)
+                .collect();
+            */
+            let data: Vec<f64> = (0..r_max).map(|x| sin(x, r_max, 10) + sin(x, r_max, 50)).collect();
+            println!(": {:?}", start.elapsed());
+            print!("fft");
+            flush();
+            let fft_res = fft::fft(&data[range.clone()]).await;
+            println!(": {:?}", start.elapsed());
+            //println!("{fft_res:#?}");
+            print!("save plot");
+            flush();
+            let amp = fft_res.iter().map(|x| x.abs() as f64).collect::<Vec<f64>>();
+            save_graph(&amp, &data[range], "./out/fourier.svg", &format!("{}: {:?}", r_max - r_min, (r_max - r_min) as f64 / info.sample_rate as f64))?;
+            println!(": {:?}", start.elapsed());
+
+            for (i, x) in amp.iter().enumerate() {
+                println!("{i:04} : {x}");
             }
-            println!("fft");
-            println!("{:?}", fft::fft(&data).await);
-
-            println!("plot vec");
-            chart
-            .draw_series(LineSeries::new(
-                (0..(data.len())).map(|x| (x as f64, data[x as usize])),
-                &RED,)
-            )?
-            .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
-
-            println!("save plot");
-            root.present()?;
-        },
+        }
         Err(error) => panic!("{error:?}"),
-    }
+    };
     Ok(())
+}
+
+
+fn sin(x: usize, max: usize, freq: usize) -> f64 {
+    ((x as f64 / max as f64) * std::f64::consts::TAU * freq as f64).sin()
 }
